0c5d26e5741b56b7d3a643e610a8815ee9a14ca7
Sander van de Graaf <github@svdgraaf.nl>
2010-11-10 16:50:11 +0100

    fix copyright copy/paste foobar

--- Classes/BakerAppDelegate.m
+++ Classes/BakerAppDelegate.m
@@ -1,9 +1,32 @@
 //
-//  PagesAppDelegate.m
-//  Pages
+//  BakerAppDelegate.m
+//  Baker
 //
-//  Created by svdgraaf on 10-11-10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  ==========================================================================================
+//  
+//  Copyright (c) 2010, Davide Casali, Marco Colombo, Alessandro Morandi
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without modification, are 
+//  permitted provided that the following conditions are met:
+//  
+//  Redistributions of source code must retain the above copyright notice, this list of 
+//  conditions and the following disclaimer.
+//  Redistributions in binary form must reproduce the above copyright notice, this list of 
+//  conditions and the following disclaimer in the documentation and/or other materials 
+//  provided with the distribution.
+//  Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to 
+//  endorse or promote products derived from this software without specific prior written 
+//  permission.
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
 #import "BakerAppDelegate.h"
@@ -23,13 +46,13 @@
     
 	self.rootViewController = [[RootViewController alloc] initWithNibName:@"RootViewController" bundle:[NSBundle mainBundle]];
 // @todo: figure out why this doesn't work :(
-//	UIView *scrollView = [rootViewController scrollView];
+	UIView *scrollView = [rootViewController scrollView];
 //
 //	// Create the application window
-//	UIWindow *localWindow = [[InterceptorWindow alloc] initWithTarget:scrollView delegate:self.rootViewController frame:[[UIScreen mainScreen]bounds]];
-//	localWindow.backgroundColor = [UIColor whiteColor];
-//	self.window = localWindow;
-//	[localWindow release];
+	UIWindow *localWindow = [[InterceptorWindow alloc] initWithTarget:scrollView delegate:self.rootViewController frame:[[UIScreen mainScreen]bounds]];
+	localWindow.backgroundColor = [UIColor whiteColor];
+	self.window = localWindow;
+	[localWindow release];
 	
     // Override point for customization after app launch. 
     [window addSubview:self.rootViewController.view];
