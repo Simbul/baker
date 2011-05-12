5f166d50aaa938ba250b6cb5ff47b43985d1d220
David Haslem <therabidbanana@gmail.com>
2011-05-12 16:30:12 -0400

    Only allow showing/hiding index bar if available (isDisabled == false)

--- Classes/IndexViewController.h
+++ Classes/IndexViewController.h
@@ -40,6 +40,7 @@
     
     int pageWidth;
 	int pageHeight;
+    BOOL disabled;
 }
 
 - (id)initWithBookBundlePath:(NSString *)path documentsBookPath:(NSString *)docpath fileName:(NSString *)name webViewDelegate:(UIViewController *)delegate;
@@ -48,6 +49,7 @@
 - (void)setBounceForWebView:(UIWebView *)webView bounces:(BOOL)bounces;
 - (void)setPageSizeForOrientation:(UIInterfaceOrientation)orientation;
 - (BOOL)isIndexViewHidden;
+- (BOOL)isDisabled;
 - (void)setIndexViewHidden:(BOOL)hidden withAnimation:(BOOL)animation;
 - (void)willRotate;
 - (void)rotateFromOrientation:(UIInterfaceOrientation)fromInterfaceOrientation toOrientation:(UIInterfaceOrientation)toInterfaceOrientation;
