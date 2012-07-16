a2d75b0ae2b58cdb6e6cddd8be1eef198715dc11
Xm4s <jintetsu@gmail.com>
2012-07-16 23:56:40 +0200

    Added ShelfViewController to the project

---
+++ BakerShelf/ShelfViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShelfViewController.h
+//  Baker
+//
+//  Created by Marco Colombo on 16/07/12.
+//  Copyright (c) 2012 Marco Natale Colombo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShelfViewController : UIViewController
+
+@end
---
+++ BakerShelf/ShelfViewController.m
@@ -0,0 +1,43 @@
+//
+//  ShelfViewController.m
+//  Baker
+//
+//  Created by Marco Colombo on 16/07/12.
+//  Copyright (c) 2012 Marco Natale Colombo. All rights reserved.
+//
+
+#import "ShelfViewController.h"
+
+@interface ShelfViewController ()
+
+@end
+
+@implementation ShelfViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
