c5d312dd98942844d563dc5cd7c7cc5ad5929a8b
jscampbell05 <james@nyxdigital.com>
2012-11-06 00:51:48 +0000

    Defines and Enums
    New Enums Created for options and Large amount of Defines moved to
    seperate file.

--- Baker/BakerViewController.m
+++ Baker/BakerViewController.m
@@ -41,37 +41,7 @@
 #import "PageTitleLabel.h"
 #import "Utils.h"
 
-// ALERT LABELS
-#define OPEN_BOOK_MESSAGE       @"Do you want to download %@?"
-#define OPEN_BOOK_CONFIRM       @"Open book"
-
-#define CLOSE_BOOK_MESSAGE      @"Do you want to close this book?"
-#define CLOSE_BOOK_CONFIRM      @"Close book"
-
-#define ZERO_PAGES_TITLE        @"Whoops!"
-#define ZERO_PAGES_MESSAGE      @"Sorry, that book had no pages."
-
-#define ERROR_FEEDBACK_TITLE    @"Whoops!"
-#define ERROR_FEEDBACK_MESSAGE  @"There was a problem downloading the book."
-#define ERROR_FEEDBACK_CONFIRM  @"Retry"
-
-#define EXTRACT_FEEDBACK_TITLE  @"Extracting..."
-
-#define ALERT_FEEDBACK_CANCEL   @"Cancel"
-
-#define INDEX_FILE_NAME         @"index.html"
-
-#define URL_OPEN_MODALLY        @"referrer=Baker"
-#define URL_OPEN_EXTERNAL       @"referrer=Safari"
-
-// SCREENSHOT
-#define MAX_SCREENSHOT_AFTER_CP  10
-#define MAX_SCREENSHOT_BEFORE_CP 10
-
 @implementation BakerViewController
-
-#pragma mark - SYNTHESIS
-@synthesize currentPageNumber;
 
 #pragma mark - INIT
 
--- BakerDefines.h
+++ BakerDefines.h
@@ -16,4 +16,32 @@
 #define SYSTEM_VERSION_LESS_THAN(version)                 ([[[UIDevice currentDevice] systemVersion] compare:version options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(version)     ([[[UIDevice currentDevice] systemVersion] compare:version options:NSNumericSearch] != NSOrderedDescending)
 
+// ALERT LABELS
+#define OPEN_BOOK_MESSAGE       @"Do you want to download %@?"
+#define OPEN_BOOK_CONFIRM       @"Open book"
+
+#define CLOSE_BOOK_MESSAGE      @"Do you want to close this book?"
+#define CLOSE_BOOK_CONFIRM      @"Close book"
+
+#define ZERO_PAGES_TITLE        @"Whoops!"
+#define ZERO_PAGES_MESSAGE      @"Sorry, that book had no pages."
+
+#define ERROR_FEEDBACK_TITLE    @"Whoops!"
+#define ERROR_FEEDBACK_MESSAGE  @"There was a problem downloading the book."
+#define ERROR_FEEDBACK_CONFIRM  @"Retry"
+
+#define EXTRACT_FEEDBACK_TITLE  @"Extracting..."
+
+#define ALERT_FEEDBACK_CANCEL   @"Cancel"
+
+#define INDEX_FILE_NAME         @"index.html"
+
+#define URL_OPEN_MODALLY        @"referrer=Baker"
+#define URL_OPEN_EXTERNAL       @"referrer=Safari"
+
+// SCREENSHOT
+#define MAX_SCREENSHOT_AFTER_CP  10
+#define MAX_SCREENSHOT_BEFORE_CP 10
+
 #endif
+
