05668a0863fe25621a19ea7ec21ddf9df0611973
Simbul <webmaster@simbul.net>
2013-02-09 00:32:13 +0000

    Fixed iOS 6.1 warnings

--- BakerShelf/ShelfViewController.m
+++ BakerShelf/ShelfViewController.m
@@ -514,12 +514,12 @@
     NSString *status = [issue getStatus];
 
     #ifdef BAKER_NEWSSTAND
-    if (status == @"opening") {
+    if ([status isEqual:@"opening"]) {
         book = [[[BakerBook alloc] initWithBookPath:issue.path bundled:NO] autorelease];
         [self pushViewControllerWithBook:book];
     }
     #else
-    if (status == @"bundled") {
+    if ([status isEqual:@"bundled"]) {
         book = [issue bakerBook];
         [self pushViewControllerWithBook:book];
     }
