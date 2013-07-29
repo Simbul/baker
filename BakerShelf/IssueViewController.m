Simbul/baker

commit 1d7f03e339264ee236f3fd07563d445ef3b63765

Take network calls off the main thread.

BakerShelf/PurchasesManager.m
@@ -202,26 +202,26 @@
     BakerAPI *api = [BakerAPI sharedInstance];
 
     if ([api canGetPurchasesJSON]) {
-        NSData* jsonResponse = [api getPurchasesJSON];
-
-        if (jsonResponse) {
-            NSError* error = nil;
-            NSDictionary *purchasesResponse = [NSJSONSerialization JSONObjectWithData:jsonResponse
-                                                                              options:0
-                                                                                error:&error];
-            // TODO: handle error
-
-            if (purchasesResponse) {
-                NSArray *purchasedIssues = [purchasesResponse objectForKey:@"issues"];
-                self.subscribed = [[purchasesResponse objectForKey:@"subscribed"] boolValue];
-
-                [productIDs enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
-                    [_purchases setObject:[NSNumber numberWithBool:[purchasedIssues containsObject:obj]] forKey:obj];
-                }];
-            } else {
-                NSLog(@"ERROR: Could not parse response from purchases API call. Received: %@", jsonResponse);
+        [api getPurchasesJSON:^(NSData* jsonResponse) {
+            if (jsonResponse) {
+                NSError* error = nil;
+                NSDictionary *purchasesResponse = [NSJSONSerialization JSONObjectWithData:jsonResponse
+                                                                                  options:0
+                                                                                    error:&error];
+                // TODO: handle error
+                
+                if (purchasesResponse) {
+                    NSArray *purchasedIssues = [purchasesResponse objectForKey:@"issues"];
+                    self.subscribed = [[purchasesResponse objectForKey:@"subscribed"] boolValue];
+                    
+                    [productIDs enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+                        [_purchases setObject:[NSNumber numberWithBool:[purchasedIssues containsObject:obj]] forKey:obj];
+                    }];
+                } else {
+                    NSLog(@"ERROR: Could not parse response from purchases API call. Received: %@", jsonResponse);
+                }
             }
-        }
+        }];
     }
 }
 

BakerShelf/BakerAPI.h
@@ -40,12 +40,12 @@
 #pragma mark - Shelf
 
 - (BOOL)canGetShelfJSON;
-- (NSData *)getShelfJSON;
+- (void)getShelfJSON:(void (^)(NSData*)) callback ;
 
 #pragma mark - Purchases
 
 - (BOOL)canGetPurchasesJSON;
-- (NSData *)getPurchasesJSON;
+- (void)getPurchasesJSON:(void (^)(NSData*)) callback ;
 
 - (BOOL)canPostPurchaseReceipt;
 - (BOOL)postPurchaseReceipt:(NSString *)receipt ofType:(NSString *)type;

BakerShelf/BakerAPI.m
@@ -55,11 +55,17 @@
 - (BOOL)canGetShelfJSON {
     return ([self manifestURL] != nil);
 }
-- (NSData *)getShelfJSON {
-    if ([self canGetShelfJSON]) {
-        return [self getFromURL:[self manifestURL] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
-    }
-    return nil;
+
+- (void)getShelfJSON:(void (^)(NSData*)) callback {
+    dispatch_queue_t currentQueue = dispatch_get_current_queue();
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSData *data = [self getFromURL:[self manifestURL] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+        if (callback) {
+            dispatch_sync(currentQueue, ^{
+                callback(data);
+            });
+        }
+    });
 }
 
 #pragma mark - Purchases
@@ -67,11 +73,22 @@
 - (BOOL)canGetPurchasesJSON {
     return ([self purchasesURL] != nil);
 }
-- (NSData *)getPurchasesJSON {
+
+- (void)getPurchasesJSON:(void (^)(NSData*)) callback  {
     if ([self canGetPurchasesJSON]) {
-        return [self getFromURL:[self purchasesURL] cachePolicy:NSURLRequestUseProtocolCachePolicy];
+        dispatch_queue_t currentQueue = dispatch_get_current_queue();
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NSData *data = [self getFromURL:[self purchasesURL] cachePolicy:NSURLRequestUseProtocolCachePolicy];
+            if (callback) {
+                dispatch_sync(currentQueue, ^{
+                    callback(data);
+                });
+            }
+        });
     }
-    return nil;
+    else if (callback) {
+        callback(nil);
+    }
 }
 
 - (BOOL)canPostPurchaseReceipt {

BakerShelf/IssuesManager.m
@@ -67,6 +67,7 @@
 -(void)refresh:(void (^)(BOOL)) callback {
     [self getShelfJSON:^(NSData* json) {
         if (json) {
+            NSError* error = nil;
             NSArray* jsonArr = [NSJSONSerialization JSONObjectWithData:json
                                                                options:0
                                                                  error:&error];
@@ -97,39 +98,36 @@
     }];
 }
 
--(void)getShelfJSON:(void (^)(NSData**)) callback {
+-(void)getShelfJSON:(void (^)(NSData*)) callback {
     BakerAPI *api = [BakerAPI sharedInstance];
-    NSData *json = [api getShelfJSON];
-
-    NSError *cachedShelfError = nil;
-
-    if (json) {
-        // Cache the shelf manifest
-        [[NSFileManager defaultManager] createFileAtPath:self.shelfManifestPath contents:nil attributes:nil];
-        [json writeToFile:self.shelfManifestPath
-                  options:NSDataWritingAtomic
-                    error:&cachedShelfError];
-        if (cachedShelfError) {
-            NSLog(@"[BakerShelf] ERROR: Unable to cache 'shelf.json' manifest: %@", cachedShelfError);
-        }
-    } else {
-        // Can't download it... Let's try to load it from previously cached version...
-        if ([[NSFileManager defaultManager] fileExistsAtPath:self.shelfManifestPath]) {
-            NSLog(@"[BakerShelf] Loading cached Shelf manifest from %@", self.shelfManifestPath);
-            json = [NSData dataWithContentsOfFile:self.shelfManifestPath options:NSDataReadingMappedIfSafe error:&cachedShelfError];
+    [api getShelfJSON:^(NSData* json) {
+        NSError *cachedShelfError = nil;
+        
+        if (json) {
+            // Cache the shelf manifest
+            [[NSFileManager defaultManager] createFileAtPath:self.shelfManifestPath contents:nil attributes:nil];
+            NSError* error = nil;
+            [json writeToFile:self.shelfManifestPath options:NSAtomicWrite error:&error];
             if (cachedShelfError) {
-                NSLog(@"[BakerShelf] Error loading cached copy of 'shelf.json': %@", cachedShelfError);
+                NSLog(@"[BakerShelf] ERROR: Unable to cache 'shelf.json' manifest: %@", cachedShelfError);
             }
         } else {
-            // Not even in cache. Bad luck.
-            NSLog(@"[BakerShelf] No cached 'shelf.json' manifest found at %@", self.shelfManifestPath);
-            json = nil;
+            if ([[NSFileManager defaultManager] fileExistsAtPath:self.shelfManifestPath]) {
+                NSLog(@"[BakerShelf] Loading cached Shelf manifest from %@", self.shelfManifestPath);
+                json = [NSData dataWithContentsOfFile:self.shelfManifestPath options:NSDataReadingMappedIfSafe error:&cachedShelfError];
+                if (cachedShelfError) {
+                    NSLog(@"[BakerShelf] Error loading cached copy of 'shelf.json': %@", cachedShelfError);
+                }
+            } else {
+                NSLog(@"[BakerShelf] No cached 'shelf.json' manifest found at %@", self.shelfManifestPath);
+                json = nil;
+            }
         }
-    }
-
-    if (callback) {
-        callback(json);
-    };
+        
+        if (callback) {
+            callback(json);
+        };
+    }];
 }
 
 -(void)updateNewsstandIssuesList:(NSArray *)issuesList {

commit 47dc0c56dea9da701469a312df54319b98f5ae69

Fixed race condition: UI vs. purchased async call
A race condition was introduced in 1d7f03e3: async call to the API `purchased` endpoint was completing *after* UI was refreshed, resulting in buttons not being updated (e.g. from "BUY" to "PURCHASED").

Fixes #1044

BakerShelf/IssueViewController.m
@@ -499,8 +499,10 @@
         }
 
         self.issue.transientStatus = BakerIssueTransientStatusNone;
-        [purchasesManager retrievePurchasesFor:[NSSet setWithObject:self.issue.productID]];
-        [self refresh];
+
+        [purchasesManager retrievePurchasesFor:[NSSet setWithObject:self.issue.productID] withCallback:^(NSDictionary *purchases) {
+            [self refresh];
+        }];
     }
 }
 - (void)handleIssuePurchaseFailed:(NSNotification *)notification {

BakerShelf/PurchasesManager.h
@@ -67,7 +67,7 @@
 - (BOOL)purchase:(NSString *)productID;
 - (BOOL)finishTransaction:(SKPaymentTransaction *)transaction;
 - (void)restore;
-- (void)retrievePurchasesFor:(NSSet *)productIDs;
+- (void)retrievePurchasesFor:(NSSet *)productIDs withCallback:(void (^)(NSDictionary*))callback;
 - (BOOL)isPurchased:(NSString *)productID;
 
 #pragma mark - Products

BakerShelf/PurchasesManager.m
@@ -205,7 +205,7 @@
     }
 }
 
-- (void)retrievePurchasesFor:(NSSet *)productIDs {
+- (void)retrievePurchasesFor:(NSSet *)productIDs withCallback:(void (^)(NSDictionary*))callback {
     BakerAPI *api = [BakerAPI sharedInstance];
 
     if ([api canGetPurchasesJSON]) {
@@ -228,6 +228,8 @@
                     NSLog(@"ERROR: Could not parse response from purchases API call. Received: %@", jsonResponse);
                 }
             }
+
+            callback([NSDictionary dictionaryWithDictionary:_purchases]);
         }];
     }
 }

BakerShelf/ShelfViewController.m
@@ -336,7 +336,9 @@
         if(status) {
             self.issues = issuesManager.issues;
 
-            [purchasesManager retrievePurchasesFor:[issuesManager productIDs]];
+            [purchasesManager retrievePurchasesFor:[issuesManager productIDs] withCallback:^(NSDictionary *purchases) {
+                // noop
+            }];
 
             [shelfStatus load];
             for (BakerIssue *issue in self.issues) {